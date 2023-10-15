3fe7bf07d711dbd65effd16a6a6ab172d8383790
Yusuf Ali <ali@yusuf.email>
2023-10-15 15:42:37 +0000

    fix: set metrics bind to 0.0.0.0 for prometheus compatibility

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -3,17 +3,14 @@ locals {
     gitlab = {
       name   = "gitlab/gitlab-ee"
       tag    = "16.4.1"
-      semvar = "~16.x.x"
     }
     coder = {
       name   = "ghcr.io/coder/coder"
-      tag    = "v2.2.1"
-      semvar = "~v2.x.x"
+      tag    = "v2.3.0"
     }
     registry = {
       name = "registry"
       tag = "2.8.3"
-      semvar = "~2"
     }
 
     databases={
@@ -22,13 +19,15 @@ locals {
       mariadb = "10.7.3"
     }
 
-    externalsecrets = "0.9.4"
-    ingress    = "4.0.6"
-    keda       = "2.10.1"
-    certmanager = "v1.12.3"
+    externalsecrets = "0.9.6"
+    ingress    = "4.7.3"
+    keda       = "2.12.0"
+    certmanager = "v1.13.1"
     nfs_provisioner = "4.0.18"
-    gitlabrunner = "v0.57.1"
     externaldns = "1.13.1"
     prometheus = "51.7.0"
+
+    gitlabrunner = "v0.57.1" # https://gitlab.com/gitlab-org/charts/gitlab-runner/-/tags
+    redis = "9.0.12"         # https://github.com/bitnami/charts/blob/main/bitnami/redis-cluster/Chart.yaml
   }
 }
